1acbd5f56307c8fcdd0397a8b248b0a8492f4b26
steagray <steagray@ut.utm.edu>
2021-10-12 19:42:43 -0500

    Added timer

--- CW9DartsForPie/Program.cs
+++ CW9DartsForPie/Program.cs
@@ -6,11 +6,13 @@ namespace CW9DartsForPie
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             int threadCount = 1;
             int dartNum = 0;
 
+            
             Console.Write("How many threads would you like to run? ");
             threadCount = int.Parse(Console.ReadLine());
             Console.Write("How many darts would you like to throw? ");
@@ -34,9 +36,12 @@ namespace CW9DartsForPie
             List<Thread> threadList = new List<Thread>();
             List<FindPiThread> FPTList = new List<FindPiThread>();
 
+            System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
+
             //creates all the threads requested from the user
             //and ties them to the subsequent FindPiThreadObject
-            for(int i = 0; i < threadCount; i++)
+            for (int i = 0; i < threadCount; i++)
             {
                 FPTList.Add(new FindPiThread(dartNum));
                 threadList.Add(new Thread(new ThreadStart(FPTList[i].throwDarts)));
@@ -50,6 +55,8 @@ namespace CW9DartsForPie
                 threadList[i].Join();
             }
 
+            stopWatch.Stop();
+
             int totalDartsInside = 0;
             //pools the data from all the FindPiThread objects
             for(int i = 0; i < threadCount; i++)
@@ -59,6 +66,7 @@ namespace CW9DartsForPie
 
             Console.Write("Pi Estimation: ");
             Console.WriteLine(((double)(4 * totalDartsInside) / (double)totalDartNum).ToString());
+            Console.WriteLine("Time Taken: {0}", stopWatch.Elapsed.ToString());
             Console.Write("Press any key to exit...");
             Console.ReadKey();
         }
