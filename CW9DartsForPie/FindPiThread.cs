abd9866cb93cd2df611dcf140819e94ea7a780b8
steagray <steagray@ut.utm.edu>
2021-10-12 19:19:02 -0500

    The assignment has been done

---
+++ CW9DartsForPie/FindPiThread.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW9DartsForPie
+{
+    class FindPiThread
+    {
+        private int totalDarts;
+        private int innerCircleDarts;
+        private Random throwGenerator;
+
+        public FindPiThread(int totalDarts)
+        {
+            this.totalDarts = totalDarts;
+            innerCircleDarts = 0;
+            throwGenerator = new Random();
+        }
+
+        public int getInnerCircleDarts()
+        {
+            return innerCircleDarts;
+        }
+
+        public void throwDarts()
+        {
+            while(totalDarts > 0)
+            {
+                double x = throwGenerator.NextDouble();
+                double y = throwGenerator.NextDouble();
+
+                if(Math.Sqrt(x * x + y * y) <= 1)
+                {
+                    innerCircleDarts += 1;
+                }
+                totalDarts--;
+            }
+        }
+    }
+}
--- CW9DartsForPie/Program.cs
+++ CW9DartsForPie/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace CW9DartsForPie
 {
@@ -6,7 +8,59 @@ namespace CW9DartsForPie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int threadCount = 1;
+            int dartNum = 0;
+
+            Console.Write("How many threads would you like to run? ");
+            threadCount = int.Parse(Console.ReadLine());
+            Console.Write("How many darts would you like to throw? ");
+            dartNum = int.Parse(Console.ReadLine());
+
+            int totalDartNum = threadCount * dartNum;
+
+            //checks to make sure inputs are valid
+            if(dartNum <= 0)
+            {
+                Console.WriteLine("Number of Darts must be greater than 0");
+                return;
+            }
+
+            if(threadCount <= 0)
+            {
+                Console.WriteLine("Number of Threads must be greater than 0");
+                return;
+            }
+
+            List<Thread> threadList = new List<Thread>();
+            List<FindPiThread> FPTList = new List<FindPiThread>();
+
+            //creates all the threads requested from the user
+            //and ties them to the subsequent FindPiThreadObject
+            for(int i = 0; i < threadCount; i++)
+            {
+                FPTList.Add(new FindPiThread(dartNum));
+                threadList.Add(new Thread(new ThreadStart(FPTList[i].throwDarts)));
+                threadList[i].Start();
+                Thread.Sleep(16);
+            }
+
+            //tells main() to wait until all threads are done before continuing
+            for(int i = 0; i < threadCount; i++)
+            {
+                threadList[i].Join();
+            }
+
+            int totalDartsInside = 0;
+            //pools the data from all the FindPiThread objects
+            for(int i = 0; i < threadCount; i++)
+            {
+                totalDartsInside += FPTList[i].getInnerCircleDarts();
+            }
+
+            Console.Write("Pi Estimation: ");
+            Console.WriteLine(((double)(4 * totalDartsInside) / (double)totalDartNum).ToString());
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
